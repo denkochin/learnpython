0f50564158a4b3f8d5e8a86c68909f02b28bba26
Denis Kochin <denkochinsp@gmail.com>
2022-02-05 13:21:53 +0300

    Работа с числами

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
-print("Hello, World!")
-for i in range(10**1000):
-    print(i)
+# Работа со строками
+phrase = "Giraffe Academy"
+print(phrase.lower())  # giraffe academy
+print(phrase.upper())  # GIRAFFE ACADEMY
+print(phrase.islower())  # False
+print(phrase.isupper())  # False
+print(len(phrase))  # 15
+print(phrase[0])  # G
+print(phrase.index("Acad"))  # 8
+print(phrase.replace("Giraffe", "Elephant"))  # Elephant Academy
+
+# Working with numbers
+print(2)
+print(2.0987)
+print(-2.0987)
+print(3 + 4)
+print(3 - 4)
+print(3 / 4)
+print(3 / 4)
+print(3 % 2)
+my_num = 5
+print(my_num)
+print(str(my_num))
+print(abs(-5))
+print(pow(2, 16))
+print(max(4, 6))
+print(min(4, 6))
+print(round(3.4), round(3.5), round(3.6), round(-0.4), round(-0.5), round(-0.6))
+from math import *
+print(ceil(3.1))
+print(floor(3.7))
+print(sqrt(64))
