b223d51c34bd77bca6ac55398c6d8f02bc9720ac
Denis Kochin <denkochinsp@gmail.com>
2022-02-05 13:36:59 +0300

    Безумная игра

--- app.py
+++ app.py
@@ -42,6 +42,13 @@ num1 = input("Enter a number: ")
 num2 = input("Enter another number: ")
 result = float(num1) + float(num2)
 print(result)
-"""
-# Mad Libs Game
 
+# Mad Libs Game
+color = input("Enter a color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love " + celebrity)
+"""
+# Lists
